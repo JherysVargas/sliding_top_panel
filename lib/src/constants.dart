b9071215f758b3c4332664a39c9c92ae55652328
Jherys Vargas <eduardo_9615@hotmail.com>
2022-03-18 19:14:20 -0500

    Delete file constants

--- lib/src/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../sliding_top_panel.dart';
-
-const Map<SlidingPanelDirection, AlignmentGeometry> directionToMap = {
-  SlidingPanelDirection.top: Alignment.topCenter,
-  SlidingPanelDirection.bottom: Alignment.bottomCenter,
-};
